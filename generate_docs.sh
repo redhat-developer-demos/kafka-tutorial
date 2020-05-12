975767bc0d64935bbb3047b36d0535507864b565
Alex Soto <asotobu@gmail.com>
2020-05-12 15:28:35 +0200

    Finishes narrative of Kafka tutorial

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -v `pwd`:/antora --rm -t antora/antora:2.2.0 --pull --stacktrace github-pages-site.yml
+docker run -v `pwd`:/antora --rm -t antora/antora:2.3.1 --pull --stacktrace github-pages-site.yml
