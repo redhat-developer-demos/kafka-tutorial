dfd0e0b51c4dd3dba552e32cd2000bf8e90c8fc0
H.P. Grahsl <grahslhp@gmail.com>
2023-01-05 15:34:57 +0100

    upgrade and improve kstream player-app

--- apps/player-app/quarkus/player-app/src/main/java/org/acme/PlaySongsGenerator.java
+++ apps/player-app/quarkus/player-app/src/main/java/org/acme/PlaySongsGenerator.java
@@ -1,34 +1,28 @@
 package org.acme;
 
-import java.time.Instant;
-import java.util.Arrays;
-import java.util.Collections;
+import java.time.Duration;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.json.bind.JsonbBuilder;
 
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
-import io.reactivex.Flowable;
+import io.quarkus.logging.Log;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.tuples.Tuple2;
 import io.smallrye.reactive.messaging.kafka.KafkaRecord;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 public class PlaySongsGenerator {
+
+    public static record Song(int id, String name, String author) {};
+
+    private static final Random RANDOM = new Random();
     
-    private static final Logger logger = LoggerFactory.getLogger(PlaySongsGenerator.class);
+    private static final List<String> USERS = List.of("Alex", "Edson", "Burr", "Sebi", "Kamesh");
 
-    private final List<String> users = Collections.unmodifiableList(
-        Arrays.asList("Alex", "Edson", "Burr", "Sebi", "Kamesh"));
-
-    private final List<Song> songs = Collections.unmodifiableList(
-        Arrays.asList(
+    private static final List<Song> SONGS = List.of(
                 new Song(1, "The Good The Bad And The Ugly", "Ennio Morricone"),
                 new Song(2, "Believe", "Cher"),
                 new Song(3, "Still Loving You", "Scorpions"),
@@ -37,59 +31,30 @@ public class PlaySongsGenerator {
                 new Song(6, "Into The Unknown", "Frozen II"),
                 new Song(7, "Fox On The Run", "Sweet"),
                 new Song(8, "Perfect", "Ed Sheeran")
-        ));
+            );
     
     // Register songs to kafka topic. Executed once.
     @Outgoing("songs")
-    public Flowable<KafkaRecord<Integer, String>> songs() {
-        final List<KafkaRecord<Integer, String>> songsAsJson = songs.stream()
-            .map(s -> KafkaRecord.of(
-                    s.id,
-                    JsonbBuilder.create().toJson(s)))
-            .peek(km -> logger.info("Registered: {}", km.getPayload()))
-            .collect(Collectors.toList());
-
-        return Flowable.fromIterable(songsAsJson);
+    public Multi<KafkaRecord<Integer, Song>> produceSongReferenceData() {
+        return Multi.createFrom().items(
+            SONGS.stream()
+                .peek(song -> Log.infof("producing -> %s", song))
+                .map(song -> KafkaRecord.of(song.id,song))
+        );
     }
 
+    // Continuously simulate random songs being played at certain intervals by users
     @Outgoing("played-songs")
-    public Flowable<KafkaRecord<Integer, String>> generatePlays() {
-        return Flowable.interval(5, TimeUnit.SECONDS)
-                .onBackpressureDrop()
-                .map(tick ->{
-                        final Song song = songs.get(random.nextInt(songs.size()));
-                        logger.info("song {}: {} played.", song.id, song.name);
-                        
-                        return KafkaRecord.of(song.id, Instant.now() + ";" + users.get(random.nextInt(users.size())));
-                });
+    public Multi<KafkaRecord<Integer, String>> generateSongPlays() {
+        return Multi.createFrom().ticks().every(Duration.ofSeconds(5))
+                .onOverflow().drop()
+                .map(tick -> Tuple2.of(
+                            SONGS.get(RANDOM.nextInt(SONGS.size())),
+                            USERS.get(RANDOM.nextInt(USERS.size()))
+                        )
+                )
+                .invoke(t -> Log.infof("Played song id %d (%s) for user %s", t.getItem1().id, t.getItem1().name, t.getItem2()))
+                .map(t -> KafkaRecord.of(t.getItem1().id,t.getItem2()));
     }
-
-    public static class Song {
-
-        int id;
-        String name;
-        String author;
-        
-        public Song(int id, String name, String author) {
-            this.id = id;
-            this.name = name;
-            this.author = author;
-        }
-    
-        public int getId() {
-            return id;
-        }
-    
-        public String getName() {
-            return name;
-        }
-    
-        public String getAuthor() {
-            return author;
-        }
-    
-    }
-
-    private Random random = new Random();
 
 }
