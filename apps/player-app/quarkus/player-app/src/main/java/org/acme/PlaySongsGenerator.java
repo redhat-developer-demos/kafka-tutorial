538d38b34234a1f288235bb1d184a7f784262e78
H.P. Grahsl <grahslhp@gmail.com>
2023-01-09 08:31:25 +0100

    add config param for song play interval

--- apps/player-app/quarkus/player-app/src/main/java/org/acme/PlaySongsGenerator.java
+++ apps/player-app/quarkus/player-app/src/main/java/org/acme/PlaySongsGenerator.java
@@ -3,9 +3,11 @@ package org.acme;
 import java.time.Duration;
 import java.util.List;
 import java.util.Random;
+import java.util.Optional;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
 import io.quarkus.logging.Log;
@@ -22,6 +24,9 @@ public class PlaySongsGenerator {
     
     private static final List<String> USERS = List.of("Alex", "Edson", "Burr", "Sebi", "Kamesh");
 
+    @ConfigProperty(name = "song.play.interval.millis")
+    public Optional<Integer> songPlayIntervalMillis;
+
     private static final List<Song> SONGS = List.of(
                 new Song(1, "The Good The Bad And The Ugly", "Ennio Morricone"),
                 new Song(2, "Believe", "Cher"),
@@ -36,6 +41,7 @@ public class PlaySongsGenerator {
     // Register songs to kafka topic. Executed once.
     @Outgoing("songs")
     public Multi<KafkaRecord<Integer, Song>> produceSongReferenceData() {
+        Log.info("creating song reference data");
         return Multi.createFrom().items(
             SONGS.stream()
                 .peek(song -> Log.infof("producing -> %s", song))
@@ -46,7 +52,9 @@ public class PlaySongsGenerator {
     // Continuously simulate random songs being played at certain intervals by users
     @Outgoing("played-songs")
     public Multi<KafkaRecord<Integer, String>> generateSongPlays() {
-        return Multi.createFrom().ticks().every(Duration.ofSeconds(5))
+        int interval = songPlayIntervalMillis.orElse(1000);
+        Log.info("starting to play random songs every "+interval+ " ms");
+        return Multi.createFrom().ticks().every(Duration.ofMillis(interval))
                 .onOverflow().drop()
                 .map(tick -> Tuple2.of(
                             SONGS.get(RANDOM.nextInt(SONGS.size())),
